04defbd139143b50198a93acbd12845147c2f539
sbconniereynolds <connie.reynolds@slalom.com>
2023-05-11 10:09:08 -0500

    update env variable that was causing errors

--- perrys-potion-palace/knexfile.js
+++ perrys-potion-palace/knexfile.js
@@ -2,22 +2,22 @@
 require("dotenv").config();
 
 const sharedConfig = {
-    client: "postgresql",
-    useNullAsDefault: true,
-    migrations: { directory: "./data/migrations" },
-    pool: { min: 2, max: 10 },
+  client: "postgresql",
+  useNullAsDefault: true,
+  migrations: { directory: "./data/migrations" },
+  pool: { min: 2, max: 10 },
 };
 
 module.exports = {
-    development: {
-        ...sharedConfig,
-        connection: {
-          user: process.env.USERNAME,
-          host: 'db.bit.io',
-          database: `${process.env.USERNAME}/${process.env.DATABASE}`,
-          password: process.env.PASSWORD,
-          port: 5432,
-          ssl: true
-        },
+  development: {
+    ...sharedConfig,
+    connection: {
+      user: process.env.USERNAME_BIT_IO,
+      host: "db.bit.io",
+      database: `${process.env.USERNAME_BIT_IO}/${process.env.DATABASE}`,
+      password: process.env.PASSWORD_BIT_IO,
+      port: 5432,
+      ssl: true,
     },
+  },
 };
